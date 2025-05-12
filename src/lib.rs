a65324045c63044a44d6c058d409500ceb49c426
mr-alimuhammadi <draco.tlw@gmail.com>
2025-05-13 00:10:48 +0330

    add pyo3_asyncio

--- src/lib.rs
+++ src/lib.rs
@@ -2,30 +2,39 @@ use edge_t2s::tts::{
     TTS,
     edge_tts::{EdgeTTS, edge_tts_config::EdgeTTSConfig},
 };
-use pyo3::prelude::*;
+
+use pyo3::{prelude::*, wrap_pyfunction};
+use pyo3_async_runtimes::tokio::future_into_py;
 
 #[pyfunction]
-async fn t2s(text: String, narrator_name: String, rate: i16, pitch: i16) -> PyResult<Vec<u8>> {
-    let tts = EdgeTTS::new(EdgeTTSConfig::new(
-        narrator_name,
-        Some(rate),
-        Some(pitch),
-        None,
-    ));
+fn t2s(
+    py: Python,
+    text: String,
+    narrator_name: String,
+    rate: i16,
+    pitch: i16,
+) -> PyResult<Bound<PyAny>> {
+    future_into_py(py, async move {
+        let tts = EdgeTTS::new(EdgeTTSConfig::new(
+            narrator_name,
+            Some(rate),
+            Some(pitch),
+            None,
+        ));
+        let mut client = tts.connect().await.map_err(|e| {
+            PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(format!("connect failed: {e}"))
+        })?;
 
-    let mut client = tts.connect().await.map_err(|e| {
-        PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(format!("TTS connect failed: {e}"))
-    })?;
+        let audio = tts.send_content(&mut client, text).await.map_err(|e| {
+            PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(format!("send_content failed: {e}"))
+        })?;
 
-    let audio = tts.send_content(&mut client, text).await.map_err(|e| {
-        PyErr::new::<pyo3::exceptions::PyRuntimeError, _>(format!("Send content failed: {e}"))
-    })?;
-
-    Ok(audio)
+        Ok(audio)
+    })
 }
 
 #[pymodule]
-fn edge_tts_py(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn edge_t2s_py(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(t2s, m)?)?;
     Ok(())
 }
